4797f7beea20863a8c99f6c571bdcc7e849defd7
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-03-30 02:13:22 -0300

    feat: implemented configurations and security improvement on cluster wolfns

---
+++ backend/wolfstore/src/main/java/com/mvbr/wolfstore/WolfstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mvbr.wolfstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WolfstoreApplication.class, args);
+    }
+
+}
